c955e39c416324398b8d1fa5a9406d574d301107
Sajadsaju <sajadsalam99@gmail.com>
2023-07-18 18:01:41 +0530

    Update Header.jsx

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { HiOutlineBell, HiOutlineChatAlt, HiOutlineSearch } from 'react-icons/hi'
-import { Popover } from '@headlessui/react'
+import { Popover, Transition,Menu } from '@headlessui/react'
+import classNames from 'classnames'
+import { useNavigate } from 'react-router-dom'
 export default function Header() {
+  const navigate= useNavigate()
   return (
     <div className='bg-white h-16 px-4 flex justify-between items-center border-b border-gray-200'>
         <div className='relative'>
@@ -15,14 +18,102 @@ export default function Header() {
         {({ open }) => (
           <>
             <Popover.Button
-              className="">
+              className={classNames (open && 'bg-gray-100',"p-1.5 rounded inline-flex items-center text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100")}>
             <HiOutlineChatAlt fontSize={24}/>
             </Popover.Button>
+
+            <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className='absolute right-0 z-10 mt-2.5 w-80'><div className='bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5'>
+           <strong className='text-gray-700 font-medium'> Messages</strong>
+           <div className='mt-2 py-1 text-sm'>
+           This is the  Message panel
+           </div>
+         
+           </div>
+           </Popover.Panel>
+</Transition>
             </> )}
           </Popover>
       
   
-        <HiOutlineBell fontSize={24} />
+       
+
+        <Popover className="relative">
+        {({ open }) => (
+          <>
+            <Popover.Button
+              className={classNames (open && 'bg-gray-100',"p-1.5 rounded inline-flex items-center text-gray-700 hover:text-opacity-100 focus:outline-none active:bg-gray-100")}>
+              <HiOutlineBell fontSize={24} />
+            </Popover.Button>
+
+            <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className='absolute right-0 z-10 mt-2.5 w-80'><div className='bg-white rounded-sm shadow-md ring-1 ring-black ring-opacity-5 px-2 py-2.5'>
+           <strong className='text-gray-700 font-medium'> Nofifications</strong>
+           <div className='mt-2 py-1 text-sm'>
+           This is the  Nofifications panel
+           </div>
+         
+           </div>
+           </Popover.Panel>
+</Transition>
+            </> )}
+          </Popover>
+      
+          <Menu as="div" className="relative">
+          <div>
+            <Menu.Button className="ml-2 inline-flex rounded-full focus:outline-none focus:ring-2 focus:ring-neutral-400">
+            <span className='sr-only'>Open Users Menu</span>
+            <div className='h-10 w-10 rounded-full bg-sky-500 bg-cover bg-no-repeat bg-center' style={{backgroundImage:'url("https://source.unsplash.com/88*88?face")'}}>
+             <span className='sr-only'>High Jack</span>
+             </div>
+             
+            </Menu.Button>
+          </div>
+          <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="origin-top-right z-10 absolute right-0 mt-2 w-48 rounded-sm shadow-md p-1 bg-white ring-1 ring-opacity-5 focus:outline-none">
+        
+              <Menu.Item>
+                {({ active }) => (
+                  <button onClick={()=>navigate('/profile')}>Your Profile</button>
+        
+                )}
+              </Menu.Item>
+
+              <Menu.Item>
+              {({ active }) => (
+                <button onClick={()=>navigate('/profile')}>Your Profile</button>
+      
+              )}
+            </Menu.Item>
+
+             
+              </Menu.Items>
+              </Transition>
+      </Menu>
         </div>
 
     </div>
