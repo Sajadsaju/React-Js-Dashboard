ec87e5e883974b4560f5dabb28a734269dcf541b
Sajadsaju <sajadsalam99@gmail.com>
2023-07-19 16:11:53 +0530

    update chart

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,7 +7,10 @@ export default function Dashboard() {
   return (
     <div className='flex flex-col gap-4'>
     <DashboardStatsGrid/>
+    <div className='flex flex-row gap-4 w-full'>
     <TransactionChart/>
+    </div>
+    
    
     </div>
   )
